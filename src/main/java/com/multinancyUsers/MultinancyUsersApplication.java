2d9fc4723e67f229537286be8c51a84cd86bc9b7
damianrein <damianrein464@gmail.com>
2024-09-03 16:35:21 -0300

    -

---
+++ src/main/java/com/multinancyUsers/MultinancyUsersApplication.java
@@ -0,0 +1,13 @@
+package com.multinancyUsers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultinancyUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultinancyUsersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/multinancyUsers/MultinancyUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multinancyUsers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultinancyUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
