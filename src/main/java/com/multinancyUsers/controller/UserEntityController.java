b15933d9c2285f4682576b95836fad29e8b65e3d
damianrein <damianrein464@gmail.com>
2024-09-08 19:27:41 -0300

    -

---
+++ src/main/java/com/multinancyUsers/controller/UserEntityController.java
@@ -0,0 +1,29 @@
+package com.multinancyUsers.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.multinancyUsers.model.UserEntity;
+import com.multinancyUsers.service.UserEntityService;
+
+@RestController
+@RequestMapping("/v1")
+public class UserEntityController {
+
+	@Autowired
+	private UserEntityService userService;
+
+	public UserEntityController(UserEntityService userService) {
+		this.userService = userService;
+	}
+	
+	@PostMapping("/")
+	public ResponseEntity<?> userRegistry(@RequestBody UserEntity user){
+		userService.registerUser(user);
+		return ResponseEntity.created(null).build();
+	}
+}
